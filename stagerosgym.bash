5b2228e280a615f666c064784849e547869f76a0
Roberto Cipollone <cipollone.rt@gmail.com>
2020-12-02 12:46:04 +0100

    Tabs helped me a lot to follow the script.

--- stagerosgym.bash
+++ stagerosgym.bash
@@ -5,193 +5,193 @@
 
 if [ "x$1" == "xstatus" ]; then
 
-echo "====================================="
-echo "Status"
-echo ""
+	echo "====================================="
+	echo "Status"
+	echo ""
 
-echo "Docker"
-docker ps
-echo "---------"
-echo ""
+	echo "Docker"
+	docker ps
+	echo "---------"
+	echo ""
 
-echo "ROS"
-docker exec -it rchomeedu-1804-melodic bash -ci "rosnode list"
-echo "---------"
-echo ""
+	echo "ROS"
+	docker exec -it rchomeedu-1804-melodic bash -ci "rosnode list"
+	echo "---------"
+	echo ""
 
-echo "====================================="
-echo ""
+	echo "====================================="
+	echo ""
 
 elif [ "x$1" == "xstart" ]; then
 
-# stage_environments
+	# stage_environments
 
-echo "====================================="
-echo "Starting stage_environments ..."
-
-
-if [ `docker image ls | grep iocchi/stage_environments | wc -l` == 0 ]; then
-
-docker pull iocchi/stage_environments
-
-fi
-
-if [ `docker container ls | grep iocchi/stage_environments | wc -l` == 0 ]; then
-
-wget -N https://bitbucket.org/iocchi/stage_environments/raw/master/docker_create.bash
-mv docker_create.bash stage_docker_create.bash
-chmod a+x stage_docker_create.bash
-./stage_docker_create.bash
-
-fi
-
-docker start stage_environments
-
-sleep 3
-
-if [ `docker ps | grep stage_environments | wc -l` == 0 ]; then
-
-echo "ERROR Container stage_environments not running"
-echo "====================================="
-echo ""
-
-exit 1
-
-fi
-
-echo "stage_environments running"
-echo "====================================="
-echo ""
+	echo "====================================="
+	echo "Starting stage_environments ..."
 
 
-# rchome_edu
+	if [ `docker image ls | grep iocchi/stage_environments | wc -l` == 0 ]; then
+
+		docker pull iocchi/stage_environments
+
+	fi
+
+	if [ `docker container ls | grep iocchi/stage_environments | wc -l` == 0 ]; then
+
+		wget -N https://bitbucket.org/iocchi/stage_environments/raw/master/docker_create.bash
+		mv docker_create.bash stage_docker_create.bash
+		chmod a+x stage_docker_create.bash
+		./stage_docker_create.bash
+
+	fi
+
+	docker start stage_environments
+
+	sleep 3
+
+	if [ `docker ps | grep stage_environments | wc -l` == 0 ]; then
+
+		echo "ERROR Container stage_environments not running"
+		echo "====================================="
+		echo ""
+
+		exit 1
+
+	fi
+
+	echo "stage_environments running"
+	echo "====================================="
+	echo ""
 
 
-echo "====================================="
-echo "Starting rc-home-edu-learn-ros ..."
-
-if [ `docker image ls | grep iocchi/rchomeedu-1804-melodic | wc -l` == 0 ]; then
-
-docker pull iocchi/rchomeedu-1804-melodic
-
-fi
-
-if [ `docker container ls | grep iocchi/rchomeedu-1804-melodic | wc -l` == 0 ]; then
-
-wget -N https://raw.githubusercontent.com/robocupathomeedu/rc-home-edu-learn-ros/master/docker/1804/create.bash
-mv create.bash rchomeedu_docker_create.bash
-chmod a+x rchomeedu_docker_create.bash
-./rchomeedu_docker_create.bash
-
-fi
-
-docker start rchomeedu-1804-melodic
-
-sleep 3
-
-if [ `docker ps | grep rchomeedu-1804-melodic | wc -l` == 0 ]; then
-
-echo "ERROR Container rchomeedu-1804-melodic not running"
-echo "====================================="
-echo ""
-exit 1
-
-fi
-
-echo "rc-home-edu-learn-ros running"
-echo "====================================="
-echo ""
+	# rchome_edu
 
 
-sleep 5
+	echo "====================================="
+	echo "Starting rc-home-edu-learn-ros ..."
 
-echo "====================================="
-echo "Starting simulation"
+	if [ `docker image ls | grep iocchi/rchomeedu-1804-melodic | wc -l` == 0 ]; then
 
-s1=stage_environments
+		docker pull iocchi/rchomeedu-1804-melodic
 
-#docker exec -it stage_environments tmux new-window -t "$s1" -n "1"
+	fi
 
-docker exec -it stage_environments tmux send-keys -t "$s1:0" "rosrun stage_environments start_simulation.py DISB1" C-M
+	if [ `docker container ls | grep iocchi/rchomeedu-1804-melodic | wc -l` == 0 ]; then
 
-echo "Simulation running"
-echo "====================================="
-echo ""
+		wget -N https://raw.githubusercontent.com/robocupathomeedu/rc-home-edu-learn-ros/master/docker/1804/create.bash
+		mv create.bash rchomeedu_docker_create.bash
+		chmod a+x rchomeedu_docker_create.bash
+		./rchomeedu_docker_create.bash
 
-sleep 5
+	fi
 
-echo "====================================="
-echo "Starting robot behaviors"
+	docker start rchomeedu-1804-melodic
 
-s2=rchomeedu
+	sleep 3
+
+	if [ `docker ps | grep rchomeedu-1804-melodic | wc -l` == 0 ]; then
+
+		echo "ERROR Container rchomeedu-1804-melodic not running"
+		echo "====================================="
+		echo ""
+		exit 1
+
+	fi
+
+	echo "rc-home-edu-learn-ros running"
+	echo "====================================="
+	echo ""
 
 
-docker exec -it rchomeedu-1804-melodic tmux new-session -d -s "$s2"
-docker exec -it rchomeedu-1804-melodic tmux send-keys -t "$s2:0"   "cd ~/src/marrtino_apps/navigation" C-M
-docker exec -it rchomeedu-1804-melodic tmux send-keys -t "$s2:0"  "roslaunch obstacle_avoidance.launch" C-M
+	sleep 5
 
-echo "Robot behaviors running"
-echo "====================================="
-echo ""
+	echo "====================================="
+	echo "Starting simulation"
+
+	s1=stage_environments
+
+	#docker exec -it stage_environments tmux new-window -t "$s1" -n "1"
+
+	docker exec -it stage_environments tmux send-keys -t "$s1:0" "rosrun stage_environments start_simulation.py DISB1" C-M
+
+	echo "Simulation running"
+	echo "====================================="
+	echo ""
+
+	sleep 5
+
+	echo "====================================="
+	echo "Starting robot behaviors"
+
+	s2=rchomeedu
+
+
+	docker exec -it rchomeedu-1804-melodic tmux new-session -d -s "$s2"
+	docker exec -it rchomeedu-1804-melodic tmux send-keys -t "$s2:0"   "cd ~/src/marrtino_apps/navigation" C-M
+	docker exec -it rchomeedu-1804-melodic tmux send-keys -t "$s2:0"  "roslaunch obstacle_avoidance.launch" C-M
+
+	echo "Robot behaviors running"
+	echo "====================================="
+	echo ""
 
 
 elif [ "x$1" == "xstop" ]; then
 
-echo "====================================="
-echo "Stopping stagerosgym application...."
+	echo "====================================="
+	echo "Stopping stagerosgym application...."
 
-docker exec -it stage_environments tmux send-keys -t "$s1:0" "rosrun stage_environments quit.sh" C-M
+	docker exec -it stage_environments tmux send-keys -t "$s1:0" "rosrun stage_environments quit.sh" C-M
 
-docker exec -it rchomeedu-1804-melodic tmux send-keys -t "$s2:0" C-c
+	docker exec -it rchomeedu-1804-melodic tmux send-keys -t "$s2:0" C-c
 
-sleep 3
+	sleep 3
 
-docker stop rchomeedu-1804-melodic
-docker stop stage_environments
+	docker stop rchomeedu-1804-melodic
+	docker stop stage_environments
 
-echo "...done"
-echo "====================================="
-echo ""
+	echo "...done"
+	echo "====================================="
+	echo ""
 
 
 
 elif [ "x$1" == "xpause" ]; then
 
-echo "====================================="
-echo "Simulation paused"
-docker exec -it stage_environments bash -ci "rostopic pub stageGUIRequest std_msgs/String   \"data: 'pause'\" --once"
-echo "====================================="
-echo ""
+	echo "====================================="
+	echo "Simulation paused"
+	docker exec -it stage_environments bash -ci "rostopic pub stageGUIRequest std_msgs/String   \"data: 'pause'\" --once"
+	echo "====================================="
+	echo ""
 
 
 
 elif [ "x$1" == "xresume" ]; then
 
-echo "====================================="
-echo "Simulation resumed"
-docker exec -it stage_environments bash -ci "rostopic pub stageGUIRequest std_msgs/String   \"data: 'resume'\" --once"
-echo "====================================="
-echo ""
+	echo "====================================="
+	echo "Simulation resumed"
+	docker exec -it stage_environments bash -ci "rostopic pub stageGUIRequest std_msgs/String   \"data: 'resume'\" --once"
+	echo "====================================="
+	echo ""
 
 
 
 elif [ "x$1" == "xspeedup" ]; then
 
-SPEEDUPVALUE=1.0
-if [ ! "$2" == "" ]; then
-  SPEEDUPVALUE=$2
-fi
+	SPEEDUPVALUE=1.0
+	if [ ! "$2" == "" ]; then
+		SPEEDUPVALUE=$2
+	fi
 
-docker exec -it stage_environments bash -ci "rostopic pub stageGUIRequest std_msgs/String   \"data: 'speedup_$SPEEDUPVALUE'\" --once"
+	docker exec -it stage_environments bash -ci "rostopic pub stageGUIRequest std_msgs/String   \"data: 'speedup_$SPEEDUPVALUE'\" --once"
 
 
 elif [ "x$1" == "xfootprints" ]; then
 
-docker exec -it stage_environments bash -ci "rostopic pub stageGUIRequest std_msgs/String   \"data: 'footprints'\" --once"
+	docker exec -it stage_environments bash -ci "rostopic pub stageGUIRequest std_msgs/String   \"data: 'footprints'\" --once"
 
 else
 
-echo "Use: $0 [start|stop|status|pause|resume|speedup <value>|footprints]"
+	echo "Use: $0 [start|stop|status|pause|resume|speedup <value>|footprints]"
 
 fi
 
